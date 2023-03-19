971db4bf279f13ac8a8d794341755ab0efb75d91
Alain Schaefer <alain.schaefer@gmail.com>
2023-03-19 14:52:32 +0100

    add no cache headers

--- TranslatorApp/TranslatorApp/kaContent.py
+++ TranslatorApp/TranslatorApp/kaContent.py
@@ -195,4 +195,9 @@ def saveData(id):
 
     return v.saveData(data)
 
-    
+@kabp.after_request
+def set_response_headers(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response   
