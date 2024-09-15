dd854ffe5785482d91a9624e89c68ecfed58fe69
Alain Schaefer <alain.schaefer@gmail.com>
2024-09-15 17:34:33 +0200

    implemented Edit & Delete for Glossary Entries and Delete for Deepl Glossary

--- TranslatorApp/TranslatorApp/glossary.py
+++ TranslatorApp/TranslatorApp/glossary.py
@@ -33,6 +33,20 @@ def deeplCreateGlossary(entries):
     else:
         return result
 
+def deeplDeleteGlossary(glossaryId):
+
+    url = "https://api-free.deepl.com/v2/glossaries/" + glossaryId
+    headers = { 'Authorization': 'DeepL-Auth-Key '+ Configuration.deeplAPI }
+    
+    result = requests.delete(url, headers=headers)
+
+    ## check the result and do proper error handling
+    if result.status_code != 200:
+        return "Error: " + result.text
+    else:
+        return result
+
+
 
 #show statistic
 bp = Blueprint('KAGlossary', __name__, url_prefix='/glossary')   
@@ -76,6 +90,37 @@ def listEntries():
     
     return jsonify(result)
 
+
+#this function creates glossary entries to the database
+@bp.route('/entries/', methods = ['POST'])
+def createEntry():
+    db = connectDB()
+    user = User(db)
+
+    if ( not user.isAdmin()):
+        print("Error: User does not have proper permissions")
+        return ""
+    
+    data = {}
+    rData = request.get_json()
+    
+    # Access individual items in the JSON array
+    data['category'] = rData['category']
+    data['source'] = rData['source']
+    data['target'] = rData['target']
+    data['comment'] = rData['comment']
+
+    cursor = db.cursor()
+    sql = "INSERT INTO %s.`ka-glossary` (%s) VALUES ('%s')" % (Configuration.dbDatabase, ', '.join(data.keys()), "', '".join(data.values()))
+    print(sql)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+            
+    db.commit()
+    db.close()
+
+    return jsonify(result)
+
 #this function saves glossary entries to the database
 @bp.route('/entries/<id>', methods = ['POST'])
 def saveData(id):
@@ -108,6 +153,26 @@ def saveData(id):
 
     return jsonify(result)
 
+@bp.route('/entries/<id>', methods = ['DELETE'])
+def deleteEntry(id):
+
+    db = connectDB()
+    user = User(db)
+
+    if ( not user.isAdmin()):
+        print("Error: User does not have proper permissions")
+        return ""
+    
+    cursor = db.cursor()
+    sql = "DELETE FROM %s.`ka-glossary` WHERE id = '%s'" % (Configuration.dbDatabase, id)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+            
+    db.commit()
+    db.close()
+
+    return jsonify(result)          
+
 #This function loads the list of Deeply Glossaries from the API
 @bp.route('/data', methods = ['GET'])
 def listGlossary():
@@ -120,7 +185,7 @@ def listGlossary():
     #return the response as json array
     return result.json()
 
-# Create a new Deepl Glossary with the .CSV file submitted trought the form
+# Create a new Deepl Glossary with the .CSV format created from database
 @bp.route('/addGlossary', methods = ['POST'])
 def addGlossary():
     message = ""
@@ -138,7 +203,6 @@ def addGlossary():
 
         # Generate TSV string
         tsv_string = "\n".join("\t".join(map(str, row.values())).strip() for row in rows)
-        #_tsv_string = "\n".join(f"{i+1}\t" + "\t".join(f'"{str(value)}"' for value in row.values()) for i, row in enumerate(rows))
         #print("TSV data with line numbers:\n", _tsv_string)
         
         dResult = deeplCreateGlossary(tsv_string)
@@ -152,6 +216,20 @@ def addGlossary():
         user=user,
         baseURL=Configuration.baseURL,
     ))
+
+@bp.route('/deleteGlossary/<id>', methods = ['DELETE'])
+def deleteGlossary(id):
+
+    db = connectDB()
+    user = User(db)
+
+    if ( not user.isAdmin()):
+        print("Error: User does not have proper permissions")
+        return ""
+    
+    result = deeplDeleteGlossary(id)
+
+    return jsonify(result)  
 
 @bp.after_request
 def set_response_headers(response):
