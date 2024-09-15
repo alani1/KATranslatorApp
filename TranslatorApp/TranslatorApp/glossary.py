0f887994a8871e7e9840be40a14a70f2da6e944c
Alain Schaefer <alain.schaefer@gmail.com>
2024-09-15 15:01:43 +0200

    adding cache-control pragma

--- TranslatorApp/TranslatorApp/glossary.py
+++ TranslatorApp/TranslatorApp/glossary.py
@@ -152,3 +152,10 @@ def addGlossary():
         user=user,
         baseURL=Configuration.baseURL,
     ))
+
+@bp.after_request
+def set_response_headers(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response  
